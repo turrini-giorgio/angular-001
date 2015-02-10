660f38ae8269e8f22c3c6f9d37efd5c9fc651b11
Giorgio Turrini <gturrini@sixtema-ict.it>
2015-02-10 09:09:13 +0100

    nothing out

---
+++ app/scripts/live.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+
+	var app = angular.module('app');
+
+	app.controller('LiveCtrl', ['$scope', '$timeout', 
+	function($scope, $timeout){
+
+
+	}]);
+})();
---
+++ app/scripts/spview.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+
+	var app = angular.module('app');
+
+	app.controller('SpviewCtrl', ['$scope', '$timeout', 
+	function($scope, $timeout){
+
+
+	}]);
+})();
